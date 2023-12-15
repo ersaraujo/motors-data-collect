1317412bff56c8cc6fff496826f09e95fd99d5c0
Elisson Rodrigo <ersa@cin.ufpe.br>
2023-12-15 03:53:52 -0300

    move and refactor files

---
+++ src/analysis/analyseMotorsControlData.py
@@ -0,0 +1,41 @@
+import numpy as np
+import utils.motorplotlib as mp
+
+if __name__ == "__main__":
+    path, formatted_datetime = mp.getMostRecentFile()
+    log = np.loadtxt(path, delimiter=',', skiprows=1)
+
+    # Indicates which motor we want to analyze.
+    which_motor = 1
+
+    # Data to plot.
+    motors = []
+    inputs = []
+    desired = []
+    timestamps = []
+
+    # Save the last input (PWM) we sent to robot.
+    last_input = -100
+
+    # Save the last timestamp of all sequence. 
+    # Used to update the initial offset for each PWM round. 
+    last_timestamp = 0
+
+    # Initial time offset of a PWM round.
+    initial_time_offset = 0
+
+    # Tolerance between two rounds of PWM's input.
+    EPS_PWM = 0.01
+
+    # Select data on log.
+    for line in log:
+        motor_data, input_data, desired_data, timestamp_data = mp.deserializeLogLineToData(line)
+        motors.append(motor_data[which_motor])
+        inputs.append(input_data[which_motor])
+        desired.append(desired_data[which_motor])
+        timestamps.append(timestamp_data)
+        
+    mp.plotMultipleGraphs(timestamps, [inputs, motors, desired], ["PWM", "current rad/s", "desired rad/s"], formatted_datetime)
+
+
+
---
+++ src/dataCollect/runMotorsCollectFromPWM.py
@@ -0,0 +1,78 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+import CommTypes_pb2 as pb
+import time
+import datetime
+import numpy as np
+import utils.utilslib as ul
+
+if __name__ ==  "__main__":
+    # CONFIGURE UDP CONNECTION
+    server = "199.0.1.1"
+    pc2robot_port = 9600
+    robot2pc_port = 9601
+    msg = pb.protoMotorsPWMSSL()
+    conn = socket(AF_INET, SOCK_DGRAM)
+    conn.bind(('', robot2pc_port))
+    conn.settimeout(0)
+
+    # CHOOSE FILE FOR PWM VALUES
+    seed = 42
+    # pwms = ul.loadPWMValues(f'./pwm_inputs/random_pwm_values_with_seed_{seed}.txt')
+    pwms = [15, -15, 0]
+    
+    # CONFIGURE TIME BETWEEN MESSAGES
+    msg_times = 3                        # REPEATS EACH MESSAGE 3 TIMES FOR RELIABILITY
+    time_between_msgs = 1                # EXECUTES EACH COMMAND FOR 1 SECONDS
+    sleep_between_iterations_ms = 0.25   # SLEEPS FOR 0.25 SECONDS AT EACH ITERATION
+    
+    log = []
+
+    for pwm in pwms:
+        msg.m1 = pwm
+        msg.m2 = pwm
+        msg.m3 = pwm
+        msg.m4 = pwm
+    
+        # START MSG TIME COUNTER
+        start = time.time()
+        while True:
+            # SLEEP
+            time.sleep(sleep_between_iterations_ms/1000)
+
+            # RECV MESSAGE
+            has_msg, current_speeds, pwms, desired_speeds, timestamp = ul.recvSSLMessage(conn)
+            if has_msg:
+                log_msg = ul.serializeMsgToLog(current_speeds, pwms, desired_speeds, timestamp)
+                log.append(log_msg)
+                print(pwms)
+
+            # COUNT TIME
+            elapsed_time = time.time() - start
+
+            # CHECK MSG SEND TIME
+            if elapsed_time > time_between_msgs:
+                # SEND AND PRINT MSG
+                for i in range(msg_times):
+                    conn.sendto(msg.SerializeToString(), (server, pc2robot_port))
+                print(f'Current msg elapsed time: {elapsed_time:.3f}')
+                print(f'{msg}')
+                break
+
+    if len(log)>0:
+        # Get the current date and time
+        current_datetime = datetime.datetime.now()
+        
+        # Format the date and time as a string
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+        path = ul.getResultsPath() + '/logs/motor_logs_' + formatted_datetime + '.csv'
+
+        print(f"Saving motors log: {len(log)}")
+        names = 'CURRENT_M1, CURRENT_M2, CURRENT_M3, CURRENT_M4, PWM_M1, PWM_M2, PWM_M3, PWM_M4, DESIRED_M1, DESIRED_M2, DESIRED_M3, DESIRED_M4, TIMESTAMP'
+        np.savetxt(path, \
+                   log,
+                   delimiter=',',
+                   header=names)
+    else:
+        print("Empty log!")
+    
+    print("Finish")
